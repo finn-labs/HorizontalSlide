2212e47ac10f5b29763f325c900ad02fc8c01380
Elvis Nuñez <elvisnunez@me.com>
2018-09-24 13:07:33 +0200

    Remove first storyboard

--- SlideRightTransition/SwipeFirstViewController.swift
+++ SlideRightTransition/SwipeFirstViewController.swift
@@ -8,70 +8,31 @@ class SwipeFirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let interactiveTransitionRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(interactiveTransitionRecognizerAction(_:)))
+        let interactiveTransitionRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(interactiveTransitionRecognizerAction(sender:)))
         interactiveTransitionRecognizer.edges = .right
-        self.view.addGestureRecognizer(interactiveTransitionRecognizer)
-    }
-    
-    @objc func interactiveTransitionRecognizerAction(_ sender: UIScreenEdgePanGestureRecognizer) {
-        if sender.state == .began {
-            self.performSegue(withIdentifier: "CustomTransition", sender: sender)
-        }
+        view.addGestureRecognizer(interactiveTransitionRecognizer)
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Endre søk", style: .done, target: self, action: #selector(changeSearchTapped(sender:)))
     }
 
-    //| ----------------------------------------------------------------------------
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "CustomTransition" {
-            let destinationViewController = segue.destination
-            
-            // Unlike in the Cross Dissolve demo, we use a separate object as the
-            // transition delegate rather then (our)self.  This promotes
-            // 'separation of concerns' as SwipeTransitionDelegate will
-            // handle pairing the correct animation controller and interaction
-            // controller for the presentation.
-            let transitionDelegate = self.customTransitionDelegate
-            
-            // If this will be an interactive presentation, pass the gesture
-            // recognizer along to our SwipeTransitionDelegate instance
-            // so it can return the necessary
-            // <UIViewControllerInteractiveTransitioning> for the presentation.
-            if let sender = sender as? UIGestureRecognizer {
-                transitionDelegate.gestureRecognizer = (sender as! UIScreenEdgePanGestureRecognizer)
-            } else {
-                transitionDelegate.gestureRecognizer = nil
-            }
-            
-            // Set the edge of the screen to present the incoming view controller
-            // from.  This will match the edge we configured the
-            // UIScreenEdgePanGestureRecognizer with previously.
-            //
-            // NOTE: We can not retrieve the value of our gesture recognizer's
-            //       configured edges because prior to iOS 8.3
-            //       UIScreenEdgePanGestureRecognizer would always return
-            //       UIRectEdgeNone when querying its edges property.
-            transitionDelegate.targetEdge = .right
-            
-            // Note that the view controller does not hold a strong reference to
-            // its transitioningDelegate.  If you instantiate a separate object
-            // to be the transitioningDelegate, ensure that you hold a strong
-            // reference to that object.
-            destinationViewController.transitioningDelegate = transitionDelegate
-            
-            // Setting the modalPresentationStyle to FullScreen enables the
-            // <ContextTransitioning> to provide more accurate initial and final
-            // frames of the participating view controllers.
-            destinationViewController.modalPresentationStyle = .fullScreen
+    @objc func changeSearchTapped(sender: Any?) {
+        let storyboard = UIStoryboard(name: "Swipe", bundle: nil)
+        let secondViewController = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SwipeSecondViewController
+        let transitionDelegate = self.customTransitionDelegate
+        if let sender = sender as? UIGestureRecognizer {
+            transitionDelegate.gestureRecognizer = (sender as! UIScreenEdgePanGestureRecognizer)
+        } else {
+            transitionDelegate.gestureRecognizer = nil
+        }
+        transitionDelegate.targetEdge = .right
+        secondViewController.transitioningDelegate = transitionDelegate
+        secondViewController.modalPresentationStyle = .fullScreen
+        present(secondViewController, animated: true, completion: nil)
+    }
+
+    @objc func interactiveTransitionRecognizerAction(sender: UIScreenEdgePanGestureRecognizer) {
+        if sender.state == .began {
+            changeSearchTapped(sender: sender)
         }
     }
-    
-    //
-    //MARK: -
-    //MARK: Unwind Actions
-    
-    //| ----------------------------------------------------------------------------
-    //! Action for unwinding from SwipeSecondViewController.
-    //
-    @IBAction func unwindToSwipeFirstViewController(_ sender: UIStoryboardSegue) {
-    }
-    
 }
