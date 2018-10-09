3dba73852bb3e9816bb9e9ba3154618d6a966080
Elvis Nuñez <elvisnunez@me.com>
2018-10-09 08:17:32 +0200

    Add some simple swiping

--- HorizontalSlide/HorizontalSlideTransitionDelegate.swift
+++ HorizontalSlide/HorizontalSlideTransitionDelegate.swift
@@ -1,8 +1,6 @@
 import UIKit
 
 public class HorizontalSlideTransitionDelegate: NSObject, UIViewControllerTransitioningDelegate {
-    var gestureRecognizer: UIPanGestureRecognizer?
-
     public func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         let presentationController = HorizontalSlideController(presentedViewController: presented, presenting: presenting)
         return presentationController
@@ -14,21 +12,5 @@ public class HorizontalSlideTransitionDelegate: NSObject, UIViewControllerTransi
 
     public func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return HorizontalSlideTransitionAnimator()
-    }
-
-    public func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        if let gestureRecognizer = gestureRecognizer {
-            return SwipeTransitionInteractionController(gestureRecognizer: gestureRecognizer)
-        } else {
-            return nil
-        }
-    }
-
-    public func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        if let gestureRecognizer = gestureRecognizer {
-            return SwipeTransitionInteractionController(gestureRecognizer: gestureRecognizer)
-        } else {
-            return nil
-        }
     }
 }
